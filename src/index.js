97628cc45b9fb4d2aa821675358b95f0e92ffada
raafa16 <raafa16@hotmail.com>
2019-02-06 15:04:22 -0500

    handle error message when user blocks location

--- src/index.js
+++ src/index.js
@@ -6,17 +6,25 @@ class App extends React.Component {
     super(props);
 
     // only time we do direct assignment to state object
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: "" };
 
     window.navigator.geolocation.getCurrentPosition(
       position => {
         this.setState({ lat: position.coords.latitude });
       },
-      err => console.log(err)
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
   render() {
-    return <div>Latitude: {this.state.lat} </div>;
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        <div>Error: {this.state.errorMessage}</div>
+      </div>
+    );
   }
 }
 
