f8e0b928cdbe2b58578845ef6225565e1824ca73
raafa16 <raafa16@hotmail.com>
2019-02-06 15:39:42 -0500

    alternate method of constructor function

--- src/index.js
+++ src/index.js
@@ -2,12 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // only time we do direct assignment to state object
-    this.state = { lat: null, errorMessage: "" };
-  }
+  state = { lat: null, errorMessage: "" };
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
