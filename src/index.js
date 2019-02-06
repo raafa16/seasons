efb65b893605777994f28d202aed51151b6243cf
raafa16 <raafa16@hotmail.com>
2019-02-06 15:36:32 -0500

    component lifecycle code refactoring

--- src/index.js
+++ src/index.js
@@ -7,16 +7,15 @@ class App extends React.Component {
 
     // only time we do direct assignment to state object
     this.state = { lat: null, errorMessage: "" };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => {
-        this.setState({ errorMessage: err.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
+
   render() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
